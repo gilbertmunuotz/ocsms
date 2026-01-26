b730838d6e7c34369340e3129c6714902804095c
brianceasar <brianceasar7@gmail.com>
2026-01-26 15:53:30 +0300

    prisma setup

--- server/prisma/migrations/20260124211819_init/migration.sql
+++ server/prisma/migrations/20260124211819_init/migration.sql
@@ -1,12 +1,33 @@
 -- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'SELLER', 'BUYER');
+
+-- CreateEnum
 CREATE TYPE "VehicleStatus" AS ENUM ('AVAILABLE', 'SOLD', 'PENDING');
 
 -- CreateEnum
 CREATE TYPE "VehicleCondition" AS ENUM ('NEW', 'USED', 'REFURBISHED');
 
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "name" DROP NOT NULL,
-ALTER COLUMN "phone_number" DROP NOT NULL;
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "phone_number" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BUYER',
+    "date_registered" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Seller" (
+    "userId" INTEGER NOT NULL,
+    "business_name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+
+    CONSTRAINT "Seller_pkey" PRIMARY KEY ("userId")
+);
 
 -- CreateTable
 CREATE TABLE "VehicleCategory" (
@@ -57,7 +78,13 @@ CREATE TABLE "Inquiry" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "VehicleCategory_category_name_key" ON "VehicleCategory"("category_name");
+
+-- AddForeignKey
+ALTER TABLE "Seller" ADD CONSTRAINT "Seller_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Vehicle" ADD CONSTRAINT "Vehicle_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "Seller"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
